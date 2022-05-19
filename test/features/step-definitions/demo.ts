54fde1b65e0a8a4cb6ad5b82eb3ab45426f39453
Cristian Silvera <cristiansilveraa@gmail.com>
2022-05-19 09:15:03 -0300

    add input handling

--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -18,7 +18,7 @@ When(/Search with (.*)/, async function(SearchItem){
 Then(/^Click on the first search result$/, async function(){
     let ele = await $(`<h3>`)
     ele.click()
-    await browser.pause(2000)
+    //await browser.pause(2000)
 })
 
 Then(/^URL should match (.*)$/, async function(ExpectedURL){
@@ -27,3 +27,38 @@ Then(/^URL should match (.*)$/, async function(ExpectedURL){
     chai.expect(url).to.equal(ExpectedURL)
 })
 
+/**
+ * Web Interactions
+ */
+Given(/^A web page is opened$/, async function(){
+    await browser.url("/inputs")
+    await browser.setTimeout({implicit: 15000, pageLoad: 10000})
+    //await browser.maximizeWindow()
+})
+
+When(/^Perform web Interactions$/, async function (){
+    /**
+     * 1. Input box
+     * Actions: 
+     * 1. Type into input box
+     * 2. Clear the fild and type or just addvalue
+     * 3. Click and type
+     * 4. Slow typiing
+     *      
+     * */
+    let num = 12345
+    let strNum = num.toString()
+    let ele = await $(`[type=number]`)
+
+    //await ele.setValue(strNum)
+    await ele.click()
+
+    for(let i =0; i<strNum.length; i++){
+        let charStr = strNum.charAt(i)
+        await browser.pause(1000)
+        await browser.keys(charStr)
+    }
+
+    //browser.pause(3000)
+    await browser.debug()
+})
